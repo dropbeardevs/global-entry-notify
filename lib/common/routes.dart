f08bdfde7e40ba1e1858354f3ce167fcd886da34
Christian Dulu <christian@chdulu.com>
2022-08-08 12:16:34 -0700

    Added Firebase Messagig

---
+++ lib/common/routes.dart
@@ -0,0 +1,10 @@
+import '../screens/homepage.dart';
+
+class Routes {
+  static var routes = {
+    '/': (context) => const HomePage(),
+    // '/ordertype': (context) => const OrderType(),
+    // '/ordercatering': (context) => const OrderCatering(),
+    // '/cart': (context) => const Cart(),
+  };
+}
